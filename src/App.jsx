1b1c0c7d9fb8bff4214b64b51bd1e0d9bf9d7ded
Spna Rai <er.sapnarai27@gmail.com>
2025-04-14 20:29:44 +0530

    vite & tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+
+  return (
+    <>
+      <div className="font-bold text-xl text-green-400">
+        Hello World
+       </div>
+    </>
+  )
+}
+
+export default App
